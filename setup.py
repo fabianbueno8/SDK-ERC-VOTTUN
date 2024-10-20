835a5bb767d14f23e87a8156db1f3c5c32af3999
fabianbueno8 <fabianbueno8@gmail.com>
2024-10-20 15:12:41 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(
+    name='erc20_sdk',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Tu Nombre',
+    description='A simple SDK for interacting with ERC20 APIs',
+    url='https://github.com/tu_usuario/erc20_sdk',  # Cambia a tu URL de GitHub
+)
